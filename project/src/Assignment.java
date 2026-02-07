af865ce1ebfca99e4edc114ed1b82f5ba0b8cada
Max5923 <maxzumi441@gmail.com>
2026-02-06 20:41:14 -0500

    Added assingments and subject classes and updated assignment

--- project/src/Assignment.java
+++ project/src/Assignment.java
@@ -1,10 +1,19 @@
 public class Assignment {
-    private boolean isCompleted;
-    private Difficulty diff;
-    private double time;
-    private int questions;
+    public boolean isCompleted;
+    public Difficulty diff;
+    public double time;
+    public int questions;
+    
+    public int id;
+    public static int nextId;
 
-    public Assignment(boolean isCompleted) {
-        
+    public Assignment(boolean isCompleted, Difficulty diff, double time, int questions) {
+        this.isCompleted = isCompleted;
+        this.diff = diff;
+        this.time = time;
+        this.questions = questions;
+
+        id = nextId;
+        nextId++;
     }
 }
---
+++ project/src/SimilarAssignments.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class SimilarAssignments {
+    ArrayList<Assignment> assignments = new ArrayList<>();
+}
