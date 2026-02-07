9a8789e7237adbda8d57c4bce7ad5f6910e68634
henrikd1215 <dabbs.3@wright.edu>
2026-02-06 21:22:16 -0500

    remove .idea, add new (empty) AddAssignmentWindow, and minor edits to MainWindow to get things working

--- project/src/MainWindow.java
+++ project/src/MainWindow.java
@@ -1,7 +1,14 @@
+import java.awt.*;
 import javax.swing.*;
-
 public class MainWindow extends JFrame {
         public MainWindow(String title) {
             super(title);
+
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel mainPanel = new JPanel();
+            mainPanel.setLayout(new BorderLayout());
+            
+            
         }
 }
