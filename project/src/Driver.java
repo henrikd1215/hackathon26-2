d5d72204b32b7e7ceb5fbe1bcf3da5b8d2f1bd4d
henrikd1215 <dabbs.3@wright.edu>
2026-02-06 20:42:21 -0500

    test

--- project/src/Driver.java
+++ project/src/Driver.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Driver {
     public static void main(String[] args) {
-        
+        MainWindow primaryWindow = new MainWindow("Homework Time Calculator");
+
+        primaryWindow.setSize(800, 500);
+        primaryWindow.setLocation(500,200);
+        primaryWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        primaryWindow.setVisible(true);
     }
 }
---
+++ project/src/MainWindow.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+        public MainWindow(String title) {
+            super(title);
+        }
+}
