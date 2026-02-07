eb675268022e540958b024f248752d4a2af9e860
Max5923 <maxzumi441@gmail.com>
2026-02-07 03:04:16 -0500

    Added JSON writing

--- src/main/java/com/middleenddevelopers/Driver.java
+++ src/main/java/com/middleenddevelopers/Driver.java
@@ -1,5 +1,9 @@
 package com.middleenddevelopers;
 
+import java.io.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.core.*;
+
 public class Driver {
     public static void main(String[] args) {
         MainWindow primaryWindow = new MainWindow("Homework Time Calculator");
@@ -7,5 +11,16 @@ public class Driver {
         primaryWindow.setSize(800, 500);
         primaryWindow.setLocation(500,200);
         primaryWindow.setVisible(true);
+
+        SubjectsHandler subjectsHandler = new SubjectsHandler();
+        ObjectMapper mapper = new ObjectMapper();
+        File f = new File("Assignments.json");
+
+        subjectsHandler.subjects.add(new Subject("Test"));
+
+        try {
+            mapper.writeValue(f, subjectsHandler);
+        } catch (Exception e) {
+        }
     }
 }
